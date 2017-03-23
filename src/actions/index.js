614afc1bd745fd3d7e195973c74b037bb857d43b
Timothy Barmann <tbarmann92@gmail.com>
2017-03-23 08:14:53 -0400

    wip

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+const ROOT_URL = 'http://redux-blog.herokuapp.com/api';
+const API_KEY = 'sdfgkkkd';
+
+export function fetchPosts() {
+ const request = axios.get(`${ROOT_URL}/posts?key=${API_KEY}`) 
+
+ return {
+  type: FETCH_POSTS,
+  payload: request
+ };
+}
