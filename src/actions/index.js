f168a782d272f754ea792a9fe10fc8e48b7d77f2
Timothy Barmann <tbarmann92@gmail.com>
2017-04-01 09:54:01 -0400

    wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+
 const ROOT_URL = 'http://redux-blog.herokuapp.com/api';
-const API_KEY = 'sdfgkkkd';
+const API_KEY = '?key=sdfgz9iqkkkd';
 
 export function fetchPosts() {
- const request = axios.get(`${ROOT_URL}/posts?key=${API_KEY}`) 
+ const request = axios.get(`${ROOT_URL}/posts${API_KEY}`) 
 
  return {
   type: FETCH_POSTS,
   payload: request
  };
+}
+
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+	return {
+		type: CREATE_POST,
+		payload: request
+	}
+
 }
