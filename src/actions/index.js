66024dc6b333bb4c7e34e085b21730e30bbae26d
Timothy Barmann <tbarmann92@gmail.com>
2017-04-02 21:06:54 -0400

    working app

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,20 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://redux-blog.herokuapp.com/api';
 const API_KEY = '?key=sdfgz9iqkkkd';
+
+export function fetchPost(id){
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+      type: FETCH_POST,
+      payload: request
+  }
+}
 
 export function fetchPosts() {
  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`) 
@@ -16,10 +27,17 @@ export function fetchPosts() {
 }
 
 export function createPost(props) {
-	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
-	return {
-		type: CREATE_POST,
-		payload: request
-	}
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
 
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
 }
